5fe42a211864ed9ce328e0e6bffa29ad486fb8a7
glbrom <golbrm@gmail.com>
2023-08-06 16:29:33 +0300

    create project v1

---
+++ ChatGPT/ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Macbook on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
